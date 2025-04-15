76f9d1f377184c7a92d86115cf6ae631a9424554
unknown <sapna.alex03@gmail.com>
2025-04-15 09:13:22 +0530

    BEWD CA2

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+
+app.get("/", (req, res)=>{
+    console.log("Server is live...");
+});
+
+app.post("/user", (req, res)=>{
+    const {email, password} = req.body;
+    if(!email || email.trim == ""){
+
+
+        res.status(400).json({ error: "Email cannot be empty."});
+    }
+    if(!password || password.trim ==""){
+        res.status(400).json({ error: "Password cannot be empty."});
+    }
+
+    const newUser ={
+        email,
+        password
+    }
+    users.push(newUser);
+    res.status(201).json({message : "User created Successfully", user: newUser});
+});
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port ${PORT}`);
+});
